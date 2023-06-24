4d4bed786647c1afed9bb15c74fcd138a44a46e6
Pavan kalyan Battu <47419169+lucky-9@users.noreply.github.com>
2023-06-24 13:13:20 +0530

    Update inputField.jsx added loading changes

--- url-shortner/src/components/inputField.jsx
+++ url-shortner/src/components/inputField.jsx
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class InputField extends Component {
-    state = { longurl:"", returnedData:""}
+    state = { longurl:"", returnedData:"", isLoading: false}
 
     handleSubmit =async (e) =>{
         e.preventDefault();
         try{
-           const {data}= await  axios.post("https://url-shortner-backend-hnpf.onrender.com/api/url", {longurl:this.state.longurl});
-           console.log(data);
-           this.setState({returnedData:data});
+            this.setState({isLoading: true});
+            this.setState({returnedData: ""});
+            const {data}= await  axios.post("https://url-shortner-backend-hnpf.onrender.com/api/url", {longurl:this.state.longurl});
+            console.log(data);
+            this.setState({returnedData:data});
+            this.setState({isLoading: false});
         }
         catch(ex){
+            this.setState({isLoading: false});
+            this.setState({returnedData: ""});
             console.log("error connecting backend!");
         }
         
@@ -32,6 +37,7 @@ class InputField extends Component {
        <p className="sub-heading">paste your long url here...</p>
         <input onChange={this.handleChange} type="text"/>
         <button type="submit" className="btn btn-primary m-3">GET</button>
+        {this.state.isLoading && <p className="sub-heading">getting the short URL...</p>}
         {this.state.returnedData && this.state.longurl&& <p className="sub-heading">{this.state.returnedData}</p>}
        </form>
       
